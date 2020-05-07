77653f7d8e710807820f5dc5f922315ca2c3eb14
Haytham Gharabawy <helgharabawy@hotmail.com>
2020-05-07 02:16:40 +0200

    adding week 2

---
+++ R_Programming_Wk2.R
@@ -0,0 +1,19 @@
+# Week 2 - R Programming
+# my first function
+
+add2 <- function(x,y) {
+  x+y
+}
+
+above10 <- function(x){
+  use <- x > 10
+  x[use]
+  
+}
+
+
+above <- function(x,n){
+  use <- x > 10
+  x[use]
+  
+}
